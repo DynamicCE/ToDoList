76aef1f2409f42d3dff232e8aee55612e5f347b5
nick.dll not found <53197797+DynamicCE@users.noreply.github.com>
2023-02-12 12:17:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputText = document.getElementById('inputText');
+addToDoButton.addEventListener('click', function() { // add butonuna tıklandığında bir fonksiyon çalıştır
+    let paragraph = document.createElement('p');  // add butonuna tıklandığında bir parağraf oluşturduk
+    toDoContainer.appendChild(paragraph); // oluşturduğumuz parağrafı toDoContainer'a ekledik
+    // üstteki koddan sonra htmlde <div id="toDoContainer"> </div> içine <p></p> eklenmiş olacak
+   for(let i = 1 ; i<= toDoContainer.childElementCount; i++)// toDoContainer'ın içindeki eleman sayısı kadar döngü oluştur 
+   {
+    paragraph.innerHTML = i + ":      " +  inputText.value ; // oluşturduğumuz parağrafın içine inputText'in değerini yazdırdık
+    } 
+    inputText.value = ''; // inputText'in değerini boşalttık
+});
+
+let clearToDoButton = document.getElementById('clearToDo'); // clear butonunu seçtik
+clearToDoButton.addEventListener('click', function() { // clear butonuna tıklandığında bir fonksiyon çalıştır
+    toDoContainer.innerHTML = ''; // toDoContainer'ın içini boşalttık 
+});
+// inputText'a yazı yazıp enter'a basınca addToDo butonuna tıklanmış gibi çalışması için
+
+inputText.addEventListener('keyup', function(event) { // inputText'e bir tuşa basıldığında bir fonksiyon çalıştır
+    if (event.key === "Enter") { // eğer basılan tuş enter ise
+        addToDoButton.click(); // addToDo butonuna tıkla
+    }
+});
+
+
